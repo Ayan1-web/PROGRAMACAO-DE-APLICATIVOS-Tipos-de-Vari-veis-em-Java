9c15bb62aad2dfeb4d17d1f5dc2d47de08f877e2
Ayan <ayancesar@gmail.com>
2026-02-22 18:17:46 -0300

    atividades 4 e 5

---
+++ atividade_4.java
@@ -0,0 +1,7 @@
+public class atividade_4{
+    public static void main(String[]args){
+        int numero = 20;
+        int dobro = numero * 2;
+        System.out.println(dobro);   
+	 }
+}
---
+++ atividade_5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class atividade_5{
+   public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Digite sua nota:");
+    float nota = sc.nextFloat();
+    System.out.println(nota);
+   sc.close();
+   }
+}
