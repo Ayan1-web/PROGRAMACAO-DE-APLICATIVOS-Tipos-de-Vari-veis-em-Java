eee95c45127587c5bc3143791b272cffa63c9515
Ayan <ayancesar@gmail.com>
2026-02-22 11:45:30 -0300

    Atividade 2 e 3

---
+++ atividade_2.java
@@ -0,0 +1,8 @@
+public class atividade_2{
+    public static void main(String[]args){
+        int numero = 20;
+        int segundonumero = 10;
+        int soma = numero + segundonumero;
+        System.out.println(soma);
+    }
+}
---
+++ atividade_3.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class atividade_3 {
+
+   public static void main(String[] var0) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Digite sua idade: ");
+      int idade = sc.nextInt();
+      System.out.println(idade);
+      sc.close();
+   }
+}
